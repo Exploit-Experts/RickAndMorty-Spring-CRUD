3e933a7878591cd5f4776b6c1fc167a23e7d33c8
BrunoMagno <bmagnoserver@gmail.com>
2024-10-26 17:57:06 -0300

    Refactor: Modify CharacterDto constructor to transform character URLs in EpisodeDto

--- src/main/java/com/rickmorty/DTO/EpisodeDto.java
+++ src/main/java/com/rickmorty/DTO/EpisodeDto.java
@@ -3,30 +3,27 @@ package com.rickmorty.DTO;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.rickmorty.Utils.Config;
-
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EpisodeDto(
-        Integer id,
 
-        String name,
-        @JsonAlias("episode")
-        String episodeCode,
-        @JsonAlias("air_date")
-        String releaseDate,
+                Integer id,
+                @JsonAlias("name")
+                String name,
+                @JsonAlias("episode") 
+                String episodeCode,
+                @JsonAlias("air_date") 
+                String releaseDate,
 
-        List<String>characters ) {
+                List<String> characters) {
         public EpisodeDto {
                 List<String> modifiedCharacters = new ArrayList<>();
 
-                characters.forEach(characterUrl ->
-                        modifiedCharacters.add(characterUrl.
-                                replace("https://rickandmortyapi.com/api/character/",
-                                        Config.base_url + "/characteres/"))
-                );
+                characters.forEach(characterUrl -> modifiedCharacters
+                                .add(characterUrl.replace("https://rickandmortyapi.com/api/character/",
+                                                Config.base_url + "/characters/")));
                 characters = modifiedCharacters;
         }
 }
