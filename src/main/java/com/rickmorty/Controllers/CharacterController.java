833eab41c3a5e0a40af0b169fe2a27526c3bb226
BrunoMagno <bmagnoserver@gmail.com>
2024-11-07 20:34:41 -0300

    Refactor: replace CharacterControl with CharacterController for improved character management

---
+++ src/main/java/com/rickmorty/Controllers/CharacterController.java
@@ -0,0 +1,39 @@
+package com.rickmorty.Controllers;
+
+import com.rickmorty.Services.CharacterService;
+import com.rickmorty.DTO.ApiResponseDto;
+import com.rickmorty.DTO.CharacterDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/characters")
+public class CharacterController {
+
+    @Autowired
+    private CharacterService characterService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponseDto<CharacterDto>> getAllCharacters(
+        @RequestParam(required = false) Integer page) {
+        ApiResponseDto<CharacterDto> characters = characterService.findAllCharacters(page);
+        return ResponseEntity.ok(characters);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CharacterDto> getCharacterById(@PathVariable String id) {
+        CharacterDto character = characterService.findACharacterById(id);
+        if (character != null) {
+            return new ResponseEntity<>(character, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/avatar/{id}")
+    public ResponseEntity<byte[]> getCharacterAvatar(@PathVariable String id) {
+        return characterService.findCharacterAvatar(id);
+    }
+}
