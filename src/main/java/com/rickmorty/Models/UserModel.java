4eec22898c1ad69bfe11159231f8d9c263f73333
Klismans-Nazario <klismansn.nazario@unifacol.edu.br>
2024-11-09 15:47:34 -0300

    feat: Favorites base structure

--- src/main/java/com/rickmorty/Models/UserModel.java
+++ src/main/java/com/rickmorty/Models/UserModel.java
@@ -3,6 +3,8 @@ package com.rickmorty.Models;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -29,6 +31,14 @@ public class UserModel {
     private LocalDateTime date_register;
     private LocalDateTime date_update;
     private LocalDateTime deleted_at;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_favorites",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "favorite_id")
+    )
+    private Set<FavoriteModel> favorites = new HashSet<>();
 
     public UserModel() {
     }
