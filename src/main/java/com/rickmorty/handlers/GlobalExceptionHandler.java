288d4bc78c32f09bcf1f956c3efd5c43fdd2f9ed
BrunoMagno <bmagnoserver@gmail.com>
2024-11-20 17:15:47 -0300

    Add ConflictException and global exception handler for conflict errors

--- src/main/java/com/rickmorty/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/rickmorty/handlers/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.rickmorty.handlers;
 import com.rickmorty.Models.CustomErrorResponse;
 import com.rickmorty.Models.ValidationErrorResponse;
 import com.rickmorty.exceptions.*;
+import com.rickmorty.exceptions.ConflictException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
-
-
 
 @Slf4j
 @ControllerAdvice
@@ -61,18 +60,22 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body("A rota " + ex.getRequestURL() + " não foi encontrada.");
     }
+
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<CustomErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
         return new ResponseEntity<>(new CustomErrorResponse(ex.getMessage()), HttpStatus.METHOD_NOT_ALLOWED);
     }
+
     @ExceptionHandler(CharacterNotFoundException.class)
     public ResponseEntity<CustomErrorResponse> handleCharacterNotFoundException(CharacterNotFoundException ex) {
         return new ResponseEntity<>(new CustomErrorResponse(ex.getMessage()), HttpStatus.NOT_FOUND);
     }
+
     @ExceptionHandler(EpisodeNotFoundException.class)
     public ResponseEntity<CustomErrorResponse> handleEpisodeNotFoundException(EpisodeNotFoundException ex) {
         return new ResponseEntity<>(new CustomErrorResponse(ex.getMessage()), HttpStatus.NOT_FOUND);
     }
+
     @ExceptionHandler(InvalidParameterException.class)
     public ResponseEntity<CustomErrorResponse> handleInvalidParameterException(InvalidParameterException ex) {
         CustomErrorResponse error = new CustomErrorResponse(ex.getMessage());
@@ -89,6 +92,11 @@ public class GlobalExceptionHandler {
         String message = "Erro no corpo da requisição: o JSON está mal formatado ou contém valores inválidos.";
         CustomErrorResponse error = new CustomErrorResponse(message);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<CustomErrorResponse> handleConflictException(ConflictException ex) {
+        return new ResponseEntity<>(new CustomErrorResponse(ex.getMessage()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(Exception.class)
